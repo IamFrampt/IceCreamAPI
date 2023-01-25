07a54e364ee77f1e83ccf3b9c9bf541f3a1b3d0b
Frampt <mikael.eriksson@iths.se>
2023-01-25 11:49:44 +0100

    added my api code

---
+++ app.js
@@ -0,0 +1,120 @@
+const express = require("express")
+const app = express()
+const bodyParser = require("body-parser")
+app.use(bodyParser.urlencoded({ extended: true }))
+
+var fs = require('fs');
+
+// file is included here:
+eval(fs.readFileSync('add.js')+'');
+
+const PORT = process.env.PORT || 3000
+
+const iceCream = {
+    allIceCreams: [
+        { ID: "1", Name: "Sandwich", Info: "Vaniljglass mellan mjuka chokladkex.", Type: "Glass", Company: "GB", nutritionalContent: { Energy: { Calorie: 187, Kilojoules: 784 }, Fat: 7.4, Salt: 0.29, Carbohydrates: 27, Protein: 3.1 } },
+        { ID: "2", Name: "Piggelin", Info: "Isglass med smak av tuttifrutti.", Type: "Pinnglass", Company: "GB", nutritionalContent: { Energy: { Calorie: 52, Kilojoules: 218 }, Fat: 0, Salt: 0.01, Carbohydrates: 13, Protein: 0 } },
+        { ID: "3", Name: "Twister", Info: "Ananasglass med jordgubbs-citronfruktis och citronfruktis med citron-limesmak.", Type: "Pinnglass", Company: "GB", nutritionalContent: { Energy: { Calorie: 69, Kilojoules: 289 }, Fat: 0.5, Salt: 0.01, Carbohydrates: 16, Protein: 0.5 } },
+        { ID: "4", Name: "Daimstrut", Info: "Vaniljglass med daimbitar i frasig strut.", Type: "Strut", Company: "GB", nutritionalContent: { Energy: { Calorie: 338, Kilojoules: 1410 }, Fat: 20, Salt: 0.31, Carbohydrates: 37, Protein: 3.9 } },
+        { ID: "5", Name: "Nogger", Info: "Vaniljglass med chokladöverdrag och nougatkärna i mitten.", Type: "Pinnglass", Company: "GB", nutritionalContent: { Energy: { Calorie: 201, Kilojoules: 834 }, Fat: 14, Salt: 0.09, Carbohydrates: 15, Protein: 2.2 } },
+        { ID: "6", Name: "88:an", Info: "Vanilj- och ischokladglass täckt med nötkrokant.", Type: "Pinnglass", Company: "GB", nutritionalContent: { Energy: { Calorie: 194, Kilojoules: 809 }, Fat: 12, Salt: 0.08, Carbohydrates: 19, Protein: 2.2 } },
+        { ID: "7", Name: "Magnum Mandel", Info: "Vaniljglass täckt med magnumchoklad och krispiga mandelbitar.", Type: "Pinnglass", Company: "GB", nutritionalContent: { Energy: { Calorie: 244, Kilojoules: 1022 }, Fat: 15, Salt: 0.08, Carbohydrates: 22, Protein: 3.5 } },
+        { ID: "8", Name: "Magnum Classic", Info: "Vaniljglass täckt med mörk mjölkchoklad.", Type: "Pinnglass", Company: "GB", nutritionalContent: { Energy: { Calorie: 239, Kilojoules: 1000 }, Fat: 14, Salt: 0.1, Carbohydrates: 24, Protein: 2.6 } },
+        { ID: "9", Name: "Magnum Vit Chocklad & Jordgubb", Info: "Jordgubbsglass med jordgubbssås, doppad i vit choklad.", Type: "Pinnglass", Company: "GB", nutritionalContent: { Energy: { Calorie: 250, Kilojoules: 1046 }, Fat: 14, Salt: 0.11, Carbohydrates: 27, Protein: 3.3 } },
+        { ID: "10", Name: "Cookie Dough", Info: "Vaniljglass med kakdeg- och chokladbitar.", Type: "Pint", Company: "Ben & Jerry", nutritionalContent: { Energy: { Calorie: 1150, Kilojoules: 4575 }, Fat: 65, Salt: 1, Carbohydrates: 130, Protein: 18 } },
+        { ID: "11", Name: "Half Baked", Info: "Choklad- och vaniljglass med brownie-,kakdeg- och chokladbitar", Type: "Pint", Company: "Ben & Jerry", nutritionalContent: { Energy: { Calorie: 1064, Kilojoules: 4487 }, Fat: 52, Salt: 1, Carbohydrates: 136, Protein: 16 } },
+        { ID: "12", Name: "Netflix & Chill'd", Info: "Gräddglass med jordnötssmör, virvel med söta och salta pretzelbitar", Type: "Pint", Company: "Ben & Jerry", nutritionalContent: { Energy: { Calorie: 1190, Kilojoules: 4678 }, Fat: 72, Salt: 0.9, Carbohydrates: 116, Protein: 22 } },
+        { ID: "13", Name: "Karamel Sutra", Info: "Choklad- och kolagräddglass med chokladbitar och i mitten en kärna av kola", Type: "Pint", Company: "Ben & Jerry", nutritionalContent: { Energy: { Calorie: 1080, Kilojoules: 4487 }, Fat: 56, Salt: 1, Carbohydrates: 128, Protein: 16 } },
+        { ID: "14", Name: "Baked Alaska", Info: "Vaniljglass med virvel av marshmallows och vitchokladbitar", Type: "Pint", Company: "Ben & Jerry", nutritionalContent: { Energy: { Calorie: 1134, Kilojoules: 4301 }, Fat: 67.2, Salt: 1, Carbohydrates: 117.6, Protein: 14.7 } },
+        { ID: "15", Name: "Phish Food", Info: "Chokladglass med marshmallows, kolasås och chokladbitar", Type: "Pint", Company: "Ben & Jerry", nutritionalContent: { Energy: { Calorie: 1170, Kilojoules: 4524 }, Fat: 55, Salt: 0.3, Carbohydrates: 155, Protein: 16 } },
+        { ID: "16", Name: "Strawberry Cheescake", Info: "Gräddglass med jordgubbscheesecake, jordgubbsbitar och en såsvirvel med grahamskex", Type: "Pint", Company: "Ben & Jerry", nutritionalContent: { Energy: { Calorie: 1040, Kilojoules: 3781 }, Fat: 58, Salt: 0.5, Carbohydrates: 114, Protein: 14 } },
+        { ID: "17", Name: "Half'n Half", Info: "", Type: "Glasspaket", Company: "Lohilo", nutritionalContent: { Energy: { Calorie: 273, Kilojoules: 1141 }, Fat: 9.28, Salt: 0.28, Carbohydrates: 22.58, Protein: 22.75 } },
+        { ID: "18", Name: "Mint Chocolate Chip", Info: "", Type: "Glasspaket", Company: "Lohilo", nutritionalContent: { Energy: { Calorie: 242, Kilojoules: 1008 }, Fat: 9.1, Salt: 0.17, Carbohydrates: 25.2, Protein: 22.75 } },
+        { ID: "19", Name: "Salted Caramel", Info: "", Type: "Glasspaket", Company: "Lohilo", nutritionalContent: { Energy: { Calorie: 261, Kilojoules: 1087 }, Fat: 9.8, Salt: 0.39, Carbohydrates: 26, Protein: 18 } },
+        { ID: "20", Name: "Sitting Bull", Info: "Vanilj & Jordgubbsglass doppad i strössel", Type: "Pinnglass", Company: "Triumf Glass", nutritionalContent: { Energy: { Calorie: 210, Kilojoules: 850 }, Fat: 12, Salt: 0.1, Carbohydrates: 22, Protein: 1.4 } },
+        { ID: "21", Name: "Calippo", Info: "Isglass med smak av cola", Type: "Glass", Company: "GB", nutritionalContent: { Energy: { Calorie: 90, Kilojoules: 390 }, Fat: 0.5, Salt: 0.01, Carbohydrates: 22, Protein: 0.5 } },
+
+    ]
+}
+
+app.get("/", (req, res) => {
+    res.sendFile(__dirname + "/public/index.html")
+})
+
+app.get("/allIceCreams", (req, res) => {
+    //var hej = iceCream.allIceCreams.sort((p1, p2) => (p1.nutritionalContent.Energy.Calorie > p2.nutritionalContent.Energy.Calorie) ? 0 : -1);
+    res.send(iceCream.allIceCreams)
+})
+
+app.get("/allIceCreams/icecream/:id", (req, res) => {
+    res.send(iceCream.allIceCreams[(req.params.id) - 1])
+})
+
+
+app.get("/allIceCreams/add", (req, res) => {  
+    
+    res.sendFile(__dirname + "/public/addData.html")
+})
+
+app.post("/allIceCreams/add", (req, res) => {  
+    let IceName = req.body.Name
+    let IceInfo = req.body.Info
+    let IceType = req.body.Type
+    let IceCompany = req.body.Company
+    let IceCalories = req.body.Calories
+    let IceKilojoules = req.body.KiloJoules
+    let IceFat = req.body.Fat
+    let IceSalt = req.body.Salt
+    let IceCarbohydrates = req.body.Carbohydrates
+    let IceProtein = req.body.Protein
+    
+
+    iceCream.allIceCreams.push({ID:"0",Name:IceName,Info:IceInfo,Type:IceType,Company:IceCompany,nutritionalContent: { Energy: { Calorie: IceCalories, Kilojoules: IceKilojoules },Fat:IceFat,Salt:IceSalt,Carbohydrates:IceCarbohydrates,Protein:IceProtein}})
+    IdIncrement();
+    res.send("Ice cream added.")
+})
+
+
+app.get("/allIceCreams/delete/:id", (req, res) => {
+
+    if(iceCream.allIceCreams[(req.params.id)-1] != null)
+    {
+        var deletedIceCream = iceCream.allIceCreams[req.params.id-1];
+        console.log(deletedIceCream)
+        iceCream.allIceCreams.splice((req.params.id)-1,1);
+        IdIncrement();
+        res.send(deletedIceCream.Name +" was deleted.")
+    }
+    else
+    {
+        res.send("No ice cream at that id")
+    }
+})
+app.use(express.static(__dirname + '/public'));
+
+const api_url = "http://localhost:3000/allicecreams/icecream/:id";
+  
+// Defining async function
+async function getapi(url) {
+    
+    // Storing response
+    const response = await fetch(url);
+    
+    // Storing data in form of JSON
+    var data = await response.json();
+    console.log(data);
+}
+// getapi(api_url);
+
+
+app.listen(PORT, () => {
+    console.log(`Hey Listen!, You're on port: ${PORT}`)
+})
+
+function IdIncrement()
+{ 
+    for(let i =0;i<iceCream.allIceCreams.length;i++)
+    {
+        iceCream.allIceCreams[i].ID = i+1; 
+    }
+}
