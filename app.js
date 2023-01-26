4d47332c7d879db7ee0103ecefad54dc676dd146
Frampt <mikael.eriksson@iths.se>
2023-01-26 11:54:04 +0100

    changed

--- app.js
+++ app.js
@@ -68,22 +68,19 @@ app.post("/allIceCreams/add", (req, res) => {
     let IceCarbohydrates = req.body.Carbohydrates
     let IceProtein = req.body.Protein
     
-
     iceCream.allIceCreams.push({ID:"0",Name:IceName,Info:IceInfo,Type:IceType,Company:IceCompany,nutritionalContent: { Energy: { Calorie: IceCalories, Kilojoules: IceKilojoules },Fat:IceFat,Salt:IceSalt,Carbohydrates:IceCarbohydrates,Protein:IceProtein}})
-    IdIncrement();
-
-    console.log(req.body);      // your JSON
-    res.send(req.body); 
-    //res.send("Ice cream added.")
+    IdIncrement()
+    
+    res.send("Ice cream added.")
 })
 
 
-app.get("/allIceCreams/delete/:id", (req, res) => {
+app.delete("/allIceCreams/delete/:id", (req, res) => {
 
     if(iceCream.allIceCreams[(req.params.id)-1] != null)
     {
         var deletedIceCream = iceCream.allIceCreams[req.params.id-1];
-        console.log(deletedIceCream)
+        //console.log(deletedIceCream)
         iceCream.allIceCreams.splice((req.params.id)-1,1);
         IdIncrement();
         res.send(deletedIceCream.Name +" was deleted.")
